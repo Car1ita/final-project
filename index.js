77cd33eecf6cbe32126fbf36214b7b59b226e951
Car1ita <carreon7@msu.edu>
2024-11-28 17:58:53 -0500

    added images, basic html for pages and started editing css for homepage

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add JavaScript below
+let currentIndex = 0;
+const slides = document.querySelectorAll('#slideshow .slide');
+
+function showNextSlide() {
+    slides[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % slides.length;
+    slides[currentIndex].classList.add('active');
+}
+
+setInterval(showNextSlide, 3000); 
+showNextSlide();
